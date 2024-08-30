6b7a75a697533db677b983ff0716c77c7b720c73
bruno fontoura <brunofontoura805@gmail.com>
2024-08-30 17:48:15 -0300

    adicionado docker-compose

--- src/database/mongo-cnnct.ts
+++ src/database/mongo-cnnct.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI: string = process.env.MONGODB_URI || ''
+const MONGODB_URI: string = process.env.MONGODB_URI || 'mongodb://mongodb:27017/shopper'
 
 export const runMongo = () => {
   mongoose.connect(MONGODB_URI)
